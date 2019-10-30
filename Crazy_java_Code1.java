7b0fac297c2401c5e2a866eaa06bbb470aca303d
rwx777 <joshuajared007@gmail.com>
2019-10-30 22:36:01 +0100

    Our first wondersome Java Code

---
+++ Crazy_java_Code1.java
@@ -0,0 +1,11 @@
+
+public class Crazy_java_Code1 {
+
+	public static void main(String[] args) {
+		
+		//Will this code run? If yes what will it return ? 
+		// \u000d System.out.println("What is this Magic ?");
+
+	}
+
+}
